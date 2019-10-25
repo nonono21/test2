e4da27a7a4c64618b56398a1c4d4a4dce5b21658
unknown <noeweber@yahoo.fr>
2019-10-26 00:52:43 +0200

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import Test from './scene.js';
-import '../App.css';
-import 'typeface-roboto';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import * as THREE from "three";
 
-  
-
-
-
-
-
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <Test />
-        <Button variant="contained" color="secondary">
-          Hello hello totooo
-        </Button>
-      </div>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.08;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    };
+    animate();
+    // === THREE.JS EXAMPLE CODE END ===
+  }
+  render() {
+    return (
+      <div />
+    )
+  }
 }
-
 export default App;
